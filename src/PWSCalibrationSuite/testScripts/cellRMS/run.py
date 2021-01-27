2d74be40819ffae9aca9c538a2b26cae89e6ab0a
Nick Anthony <nicholas.anthony@northwestern.edu>
2021-01-27 11:17:47 -0600

    improve plotting

--- src/PWSCalibrationSuite/testScripts/cellRMS/run.py
+++ src/PWSCalibrationSuite/testScripts/cellRMS/run.py
@@ -34,44 +34,34 @@ if __name__ == '__main__':
     for param in['rms', 'reflectance']:
         pChange = df2.groupby(['cellNum', 'roiNum']).apply(lambda g: 100 * (g[param] - ref.loc[g.name][param]) / ref.loc[g.name][param])
         pChange.index = pChange.index.get_level_values(-1)
-        newName = f'pChange_{param}'
+        newName = f'% Change {param}'
         df2[newName] = pChange
         fig, ax = plt.subplots()
         fig.suptitle(f"Aligned Stability of {param}")
         sns.boxplot(data=df2, x='experiment', y=newName)
-
+        # sns.scatterplot(data=df2, x='experiment', y=newName)
 
     # Calculate percent change of rms and reflectance
-    df2 = df.loc[:, ['experiment', 'setting', 'cellNum', 'roiNum', 'rms', 'reflectance']]
+    df2 = df.loc[:, ['experiment', 'setting', 'cellNum', 'roiNum', 'rms', 'reflectance', 'isref']]
     df2['condition'] = df2.apply(lambda row: f"{row['experiment']}.{row['setting']}", axis=1)
     ref = df2[(df2['experiment'] == 'centered') & (df2['setting'] == '0_52')].set_index(['cellNum', 'roiNum'])
     for param in ['rms', 'reflectance']:
-        pChange = df2.groupby(['cellNum', 'roiNum']).apply(lambda g: 100 * (g[param] - ref.loc[g.name][param]) / ref.loc[g.name][param])
+        df3 = df2.copy()
+        pChange = df3.groupby(['experiment', 'cellNum', 'roiNum']).apply(lambda g: 100 * (g[param] - g[g.isref].iloc[0][param]) / g[g.isref].iloc[0][param])
         pChange.index = pChange.index.get_level_values(-1)
         newName = f'pChange_{param}'
-        df2[newName] = pChange
-        fig, ax = plt.subplots()
-        sns.boxplot(data=df2, x='condition', y=newName)
-        plt.xticks(rotation=30)
+        df3[newName] = pChange
+        df3 = df3[~df3['isref']]  # Data is normalized by the references so don't bother plotting them.
+        # fig, ax = plt.subplots()
+        # sns.boxplot(data=df3, x='condition', y=newName)
+        # plt.xticks(rotation=30)
         fig, axs = plt.subplots(ncols=3, sharey=True)
-        for i, (expName, g) in enumerate(df2.groupby('experiment')):
+        fig.suptitle(f"% Change {param.upper()}")
+        for i, (expName, g) in enumerate(df3.groupby('experiment')):
             ax = axs[i]
             sns.boxplot(data=g, x='setting', y=newName, ax=ax)
             ax.set(ylabel=None, xlabel=expName)
             ax.grid(axis='y')
-        # plt.tight_layout()
         plt.subplots_adjust(wspace=0)
-
-
-
-
-    # plt.figure()
-    # sns.boxplot(data=df2, x='experiment', y='pChange_rms', hue='setting')
-    #
-    # plt.figure()
-    # sns.boxplot(data=df2, x='experiment', y='pChange_reflectance', hue='setting')
-
-    # for i, g in df.groupby(['experiment', 'setting']):
-
 
     a = 1
