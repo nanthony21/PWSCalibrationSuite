77aa0962b08ee3a67824c1aa2f2e355236c67214
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2021-05-11 13:18:19 -0500

    work towards a functioning loader for the GUI

--- src/pws_calibration_suite/loader.py
+++ src/pws_calibration_suite/loader.py
@@ -1,42 +1,47 @@
 import os
 import typing
+
+import pwspy
+
 from pws_calibration_suite.comparison import ITOMeasurement, AbstractMeasurementLoader
 import pwspy.analysis.pws as pwsAnalysis
 import pwspy.dataTypes as pwsdt
+import pathlib as pl
 
-
-class Loader(AbstractMeasurementLoader):
+#TODO, copied not changed.
+class DefaultLoader(AbstractMeasurementLoader):
     """
-    An ITO calibration loader for this experiment. The reference for each ITO acquisiton is Cell3 from the same experiemental condition
+    An ITO calibration loader for the automated calibration acquisition.
 
     """
     analysisSettings = pwsAnalysis.PWSAnalysisSettings.loadDefaultSettings("Recommended")
+    templateDir = pl.Path.home() / 'PwspyApps' / 'PWSCalibrationSuite' / 'template'
 
-    def __init__(self, rootDir: str, measurementSetName: str, readOnly: bool = False):
+    def __init__(self, rootDir: os.PathLike, readOnly: bool = False):
         """
 
         Args:
             rootDir:
-            measurementSetName:
             readOnly: If true then an exception will be thrown if the calibration files don't already exist. Otherwise new calibration files be begin being created.
         """
-        meas = self.generateITOMeasurements(rootDir, measurementSetName, readOnly)
-        template = [m for m in meas if m.name == 'centered_0_52'][0]
-        self._template = template
-        self._measurements = meas
+        self._measurements = self.generateITOMeasurements(rootDir, readOnly)
+        self._template = ITOMeasurement(self.templateDir,
+                                  pwsdt.AcqDir(self.templateDir / 'Cell1'),
+                                  pwsdt.AcqDir(self.templateDir / 'Cell3'),
+                                  self.analysisSettings,
+                                  "ITO",
+                                  readOnly=False)
 
     @classmethod
-    def generateITOMeasurements(cls, rootDir: str, measurementSetName: str, readOnly: bool):
-        measurements = []
-        for expType in ['centered', 'fieldstop', 'translation']:
-            for condition in os.listdir(os.path.join(rootDir, expType)):
-                if os.path.isdir(os.path.join(rootDir, expType, condition)):
-                    itoAcq = pwsdt.AcqDir(os.path.join(rootDir, expType, condition, 'ito', 'Cell1'))
-                    refAcq = pwsdt.AcqDir(os.path.join(rootDir, expType, condition, 'cells', "Cell3"))
-                    name = f"{expType}_{condition}"
-                    homeDir = os.path.join(rootDir, "calibrationResults", measurementSetName, name)
-                    measurements.append(ITOMeasurement(homeDir, itoAcq, refAcq, cls.analysisSettings, name, readOnly=readOnly))
-        return measurements
+    def generateITOMeasurements(cls, rootDir: os.PathLike, readOnly: bool):
+        seq, acqs = pwspy.utility.acquisition.loadDirectory(rootDir)
+        acqs = [acq.acquisition for acq in acqs]
+        itoAcq, refAcq, glassAcq, scatterAcq = acqs
+
+        name = f"ITO"
+        homeDir = os.path.join(rootDir, "calibrationResults", name)
+        measurement = ITOMeasurement(homeDir, itoAcq, refAcq, cls.analysisSettings, name, readOnly=readOnly)
+        return [measurement]
 
     @property
     def template(self) -> ITOMeasurement:
